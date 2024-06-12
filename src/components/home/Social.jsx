4b8364b82edeea9a37d7cdcc369eca3627c4d04d
sujeet Kumar <99119279+solosahej@users.noreply.github.com>
2024-06-12 18:06:08 +0530

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,13 +4,13 @@ import { FiTwitter, FiGithub, FiLinkedin} from "react-icons/fi";
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://twitter.com/solosahej" className="home__social-icon" target="_blank">
+            <a href="https://twitter.com/solosahej" className="home__social-icon" target="_blank" rel="noreferrer">
                 <FiTwitter />
             </a>
-            <a href="https://www.github.com/solosahej" className="home__social-icon" target="_blank">
+            <a href="https://www.github.com/solosahej" className="home__social-icon" target="_blank" rel="noreferrer">
                 <FiGithub />
             </a>
-            <a href="https://www.linkedin.com/in/solosahej/" className="home__social-icon" target="_blank">
+            <a href="https://www.linkedin.com/in/solosahej/" className="home__social-icon" target="_blank" rel="noreferrer">
                 <FiLinkedin />
             </a>
         </div> 
